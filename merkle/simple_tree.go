9ac4c56a83513ce5538e7a573d86c33fd3747aad
Ethan Buchman <ethan@coinculture.info>
2018-05-22 12:12:41 -0400

    remove unused simple_tree funcs

--- merkle/simple_tree.go
+++ merkle/simple_tree.go
@@ -28,16 +28,40 @@ import (
 	"github.com/tendermint/tmlibs/merkle/tmhash"
 )
 
-// Append the two hashes and hash them
-func SimpleHashFromTwoHashes(left []byte, right []byte) []byte {
+// Hash(left | right). The basic operation of the Merkle tree.
+func SimpleHashFromTwoHashes(left, right []byte) []byte {
 	var hasher = tmhash.New()
 	hasher.Write(left)
 	hasher.Write(right)
 	return hasher.Sum(nil)
 }
 
+// Compute a Merkle tree from items that can be hashed.
+func SimpleHashFromHashers(items []Hasher) []byte {
+	hashes := make([][]byte, len(items))
+	for i, item := range items {
+		hash := item.Hash()
+		hashes[i] = hash
+	}
+	return simpleHashFromHashes(hashes)
+}
+
+// Compute a Merkle tree from sorted map.
+// Like calling SimpleHashFromHashers with
+// `item = []byte(Hash(key) | Hash(value))`,
+// sorted by `item`.
+func SimpleHashFromMap(m map[string]Hasher) []byte {
+	sm := newSimpleMap()
+	for k, v := range m {
+		sm.Set(k, v)
+	}
+	return sm.Hash()
+}
+
+//----------------------------------------------------------------
+
 // Expects hashes!
-func SimpleHashFromHashes(hashes [][]byte) []byte {
+func simpleHashFromHashes(hashes [][]byte) []byte {
 	// Recursive impl.
 	switch len(hashes) {
 	case 0:
@@ -45,43 +69,8 @@ func SimpleHashFromHashes(hashes [][]byte) []byte {
 	case 1:
 		return hashes[0]
 	default:
-		left := SimpleHashFromHashes(hashes[:(len(hashes)+1)/2])
-		right := SimpleHashFromHashes(hashes[(len(hashes)+1)/2:])
+		left := simpleHashFromHashes(hashes[:(len(hashes)+1)/2])
+		right := simpleHashFromHashes(hashes[(len(hashes)+1)/2:])
 		return SimpleHashFromTwoHashes(left, right)
 	}
-}
-
-// NOTE: Do not implement this, use SimpleHashFromByteslices instead.
-// type Byteser interface { Bytes() []byte }
-// func SimpleHashFromBytesers(items []Byteser) []byte { ... }
-
-func SimpleHashFromByteslices(bzs [][]byte) []byte {
-	hashes := make([][]byte, len(bzs))
-	for i, bz := range bzs {
-		hashes[i] = SimpleHashFromBytes(bz)
-	}
-	return SimpleHashFromHashes(hashes)
-}
-
-func SimpleHashFromBytes(bz []byte) []byte {
-	hasher := tmhash.New()
-	hasher.Write(bz)
-	return hasher.Sum(nil)
-}
-
-func SimpleHashFromHashers(items []Hasher) []byte {
-	hashes := make([][]byte, len(items))
-	for i, item := range items {
-		hash := item.Hash()
-		hashes[i] = hash
-	}
-	return SimpleHashFromHashes(hashes)
-}
-
-func SimpleHashFromMap(m map[string]Hasher) []byte {
-	sm := NewSimpleMap()
-	for k, v := range m {
-		sm.Set(k, v)
-	}
-	return sm.Hash()
 }
